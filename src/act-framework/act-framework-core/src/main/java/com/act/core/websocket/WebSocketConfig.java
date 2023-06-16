71de40661d339d4cd3b8f8d3c10920dc7bdc71b1
phil.guo <phil.guo@hg2mes.com>
2023-06-16 17:46:07 +0800

    websocket 测试成功

--- src/act-framework/act-framework-core/src/main/java/com/act/core/websocket/WebSocketConfig.java
+++ src/act-framework/act-framework-core/src/main/java/com/act/core/websocket/WebSocketConfig.java
@@ -3,12 +3,14 @@ package com.act.core.websocket;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 @Configuration
+@EnableWebSocket
 public class WebSocketConfig implements ServletContextInitializer {
 
     /**
