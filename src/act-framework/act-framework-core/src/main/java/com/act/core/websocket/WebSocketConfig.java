f1bab4103ec845ebd456b79410b93ff0e0031464
1228115857 <1228115857@qq.com>
2023-09-17 21:09:00 +0800

    修改pom文件配置 设置启动项目位jar包

--- src/act-framework/act-framework-core/src/main/java/com/act/core/websocket/WebSocketConfig.java
+++ src/act-framework/act-framework-core/src/main/java/com/act/core/websocket/WebSocketConfig.java
@@ -1,28 +1,28 @@
-package com.act.core.websocket;
-
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
-@Configuration
-@EnableWebSocket
-public class WebSocketConfig implements ServletContextInitializer {
-
-    /**
-     * 这个bean的注册,用于扫描带有@ServerEndpoint的注解成为websocket,如果你使用外置的tomcat就不需要该配置文件
-     */
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
-    }
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-
-    }
-}
+//package com.act.core.websocket;
+//
+//import org.springframework.boot.web.servlet.ServletContextInitializer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.config.annotation.EnableWebSocket;
+//import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+//
+//import javax.servlet.ServletContext;
+//import javax.servlet.ServletException;
+//
+//@Configuration
+//@EnableWebSocket
+//public class WebSocketConfig implements ServletContextInitializer {
+//
+//    /**
+//     * 这个bean的注册,用于扫描带有@ServerEndpoint的注解成为websocket,如果你使用外置的tomcat就不需要该配置文件
+//     */
+//    @Bean
+//    public ServerEndpointExporter serverEndpointExporter() {
+//        return new ServerEndpointExporter();
+//    }
+//
+//    @Override
+//    public void onStartup(ServletContext servletContext) throws ServletException {
+//
+//    }
+//}
