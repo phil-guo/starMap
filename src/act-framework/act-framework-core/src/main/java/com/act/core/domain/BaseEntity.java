896b152a69c8e58a20eff2861dba707777261005
1228115857 <1228115857@qq.com>
2023-09-23 17:03:09 +0800

    去掉时间格式中的T

--- src/act-framework/act-framework-core/src/main/java/com/act/core/domain/BaseEntity.java
+++ src/act-framework/act-framework-core/src/main/java/com/act/core/domain/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.act.core.domain;
 
 import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -15,8 +16,11 @@ import java.util.Date;
 public abstract class BaseEntity<T> implements Serializable {
     @TableId(type = IdType.AUTO)
     private T id;
+
     @TableField(value = "createTime", fill = FieldFill.INSERT)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createTime = LocalDateTime.now();
+
     @TableField(value = "isDelete", fill = FieldFill.INSERT)
     @TableLogic
     private Integer isDelete = 0;
